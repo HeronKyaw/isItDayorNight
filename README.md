942a6fcf6f60e291aef73ece541e7148c8b31969
Wunna Kyaw <heronskyaw@gmail.com>
2022-02-20 16:22:46 +0630

    Added afternoon and evening

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # isItDayorNight
 This project is made with HTML, CSS and Django, and it is inspired by isitchristmas.com.
 This projects uses local time, and provides us whether it is day or night time.
+
+UPDATE: add afternoon and evening time.
--- isitdayornight/dayornight/views.py
+++ isitdayornight/dayornight/views.py
@@ -6,5 +6,8 @@ def index(request):
     now = datetime.now()
     currentTime = int(now.strftime("%H"))
     return render(request, "dayornight/index.html", {
-        "dayornight" : currentTime < 12 and currentTime > 12
+        "morning" : 6 < currentTime < 12,
+        "afternoon" : 12 < currentTime < 17,
+        "evening" : 17 < currentTime < 22,
+        "night" : 22 < currentTime < 6
     })
